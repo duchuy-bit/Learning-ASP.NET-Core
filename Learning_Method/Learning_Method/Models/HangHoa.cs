06f4520cb41f4c502afc727cc2fd65988449fdd5
Duc Huy <55582430+duchuy-bit@users.noreply.github.com>
2023-11-14 16:46:15 +0700

    Create API

---
+++ Learning_Method/Learning_Method/Models/HangHoa.cs
@@ -0,0 +1,13 @@
+﻿namespace Learning_Method.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public int Dongia { get; set; }
+    }
+
+    public class HangHoa: HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
